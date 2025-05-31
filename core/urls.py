84dc512adab40bd993c0450c6baf800b2d66c23a
Saksham <123803359+SakshamDev2005@users.noreply.github.com>
2025-05-31 20:33:51 +0530

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'), # About page
+    path('contact/', views.contact, name='contact'), # Contact page
+    path('upload_csv/', views.upload_csv, name='upload_csv'), # CSV file upload page
+    path('terms/', views.terms, name='terms'), # Terms of service page
+    path('privacy/', views.privacy, name='privacy'), # Privacy policy page
+    path('generate_plot/', views.generate_plot, name='generate_plot'), # Generate plot page
+    path('clear-session/', views.clear_session, name='clear_session'), # Clear session data
+]
